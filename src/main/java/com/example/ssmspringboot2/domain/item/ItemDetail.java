fb55afecaa89c7cb8aadf7d8d5eba8c9b679f972
mornyang <1224734406@qq.com>
2018-07-11 17:01:32 +0800

    [add] item module

--- src/main/java/com/example/ssmspringboot2/domain/item/ItemDetail.java
+++ src/main/java/com/example/ssmspringboot2/domain/item/ItemDetail.java
@@ -1,4 +1,8 @@
 package com.example.ssmspringboot2.domain.item;
 
 public class ItemDetail {
+    private Integer id;
+    private String name;
+    private Integer fling_power;
+    private Integer cost;
 }
